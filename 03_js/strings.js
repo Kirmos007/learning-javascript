d659b42ba6f42be62c9ae116ce8ba8b36a09c262
Somrik Tapaswi <109235095+Kirmos007@users.noreply.github.com>
2024-05-13 07:45:37 +0000

    strings and their required in built methods

---
+++ 03_js/strings.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let filename = "movie-1-mp4"
+let fileNo = 1
+let owner = "  Somrik  Tapaswi  "
+
+// string concatenation
+// console.log(owner + " wants to open file no. " + fileNo + " in order to watch " + filename);
+// string interpolation
+// console.log(`${owner} wants to open file no. ${fileNo} in order to watch ${filename}`) 
+
+// console.log(filename.length);
+// console.log(filename.toUpperCase());
+// console.log(filename.toLowerCase());
+// console.log(filename.replace("-",".")) // replaces only the first occurence of the given sequence
+// console.log(filename.charAt("1"))
+// console.log(filename.indexOf("o"))
+
+// slice method : performs string slicing operation
+// console.log(filename.slice(0,4))
+// console.log(filename.slice(-11, -7))
+
+// trim method : only removes blank space from either front or back or both the side of a string but fails
+// to remove blank spaces in between words of a sentence 
+// console.log(owner)
+// console.log(owner.trim())
+// console.log(owner.trimEnd())
+// console.log(owner.trimStart())
+
+// split method : splits a string into array of strings based on a given character.
+// console.log(owner.split(" ")) 
+// console.log(filename.split("-")); // splits a string based on the given character(-)
