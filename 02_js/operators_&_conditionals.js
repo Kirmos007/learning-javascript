54eba08f2aa8b8eb594b1ef549a358248ae217c3
Somrik Tapaswi <109235095+Kirmos007@users.noreply.github.com>
2024-05-14 08:50:43 +0000

    arithmetic operators and other operators via assignment questions

---
+++ 02_js/operators_&_conditionals.js
@@ -0,0 +1,76 @@
+// arithmetic operator
+
+let a = 10
+let b = 5
+
+// console.log(a+b); // addition
+// console.log(a-b); // gives the subtraction
+// console.log(a*b); // gives the multiplication a & b 
+// console.log(a/b); // gives the quotient when a is divided by b
+// console.log(a%b); // gives reminder when a is divided by b
+// console.log(a**b); // performs a to the power b
+// console.log(++a); // pre increment operator
+// console.log(--b); // pre decrement 
+// console.log(a++); // post increment
+// console.log(b++); // post decrement
+
+// assignments
+
+// // q1-> find if the age of a person is between 10 and 20 or not 
+
+// let ageOfPerson_1 = 15
+// let ageOfPerson_2 = 5
+// let ageOfPerson_3 = 25
+
+// const checkAge =(age)=>{
+//     if (age >= 10 &&  age <= 20) return "true"
+//     else return false
+// }
+
+// console.log(checkAge(ageOfPerson_1));
+// console.log(checkAge(ageOfPerson_2));
+// console.log(checkAge(ageOfPerson_3));
+
+// use of switch case statements  : to be done later
+
+
+
+// // q3-> check if a no is divisible by both 2 and 3
+
+// let num_1 = 6
+// let num_3 = 8
+// let num_4 = 9
+// let num_2 = 5
+
+// const checkDiv =(n)=>{
+//     if (n%2 == 0 && n%3 == 0) return "divisible by both"
+//     else if(n%2 == 0) return "divisible by 2"
+//     else if(n%3 == 0) return "divisible by 3"
+//     else return "div by none"
+// }
+
+// console.log(checkDiv(num_1));
+// console.log(checkDiv(num_2));
+// console.log(checkDiv(num_3));
+// console.log(checkDiv(num_4));
+
+// // q4-> check if the num is div by 2 or 3
+
+// const checkDiv2 = (x)=>{
+//     if (x%2 == 0 || x%3 == 0) return "div by either of them"
+//     else  return "div by none"
+// }
+
+// console.log(checkDiv2(num_3));
+// console.log(checkDiv2(num_4));
+// console.log(checkDiv2(num_2));
+
+// // q5-> check if a person is eligible for driving or not 
+
+// const checkDrive =(age)=>{
+//     if (age < 18) return "can not drive"
+//     else return "can drive"
+// }
+
+// console.log(checkDrive(ageOfPerson_3));
+// console.log(checkDrive(ageOfPerson_2));
