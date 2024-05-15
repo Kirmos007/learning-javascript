bd9f66f4817054bbefa7fd5d9f42626e8c14e1f6
Somrik Tapaswi <109235095+Kirmos007@users.noreply.github.com>
2024-05-15 08:43:24 +0000

    arrays in js

---
+++ 03_js/arrays.js
@@ -0,0 +1,9 @@
+// JS arrays
+
+// example of arrays
+
+const arr_1 = ["mango", "orange", "jack fruit", "watermelon"]
+const arr_2 = [5, true, false, "somrik"]
+
+console.log(arr_1);
+console.log(arr_2);
