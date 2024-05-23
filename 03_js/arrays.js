72f84d1d6ca2f82421f403705ac614e44a600bde
Somrik Tapaswi <109235095+Kirmos007@users.noreply.github.com>
2024-05-23 16:21:41 +0000

    arrays in js

--- 03_js/arrays.js
+++ 03_js/arrays.js
@@ -2,8 +2,43 @@
 
 // example of arrays
 
-const arr_1 = ["mango", "orange", "jack fruit", "watermelon"]
-const arr_2 = [5, true, false, "somrik"]
+const arr_1 = ["mango", "orange", "jack fruit", "watermelon"] // array 1
+const arr_2 = [5, true, false, "somrik"] // array 2
 
-console.log(arr_1);
-console.log(arr_2);
+// console.log(typeof arr_1);
+// console.log(arr_2);
+// console.log(arr_1[0]); //accessing the first element of array
+// console.log(arr_1[arr_1.length-1]); //accessing last element of an array
+
+// new_arr = arr_1 // creates shallow copy
+
+// // push and pop
+
+// arr_1.push("grapes") // inserts an element at the end of an array
+// console.log(arr_1);  
+
+// console.log(arr_1.pop()); // removes an element from the end of an array
+
+// // shift and unshift
+
+// arr_1.unshift("grapes") // inserts an element at the beginning of an array
+// console.log(arr_1); 
+
+// console.log(arr_1.shift()); // removes an element from starting of an array
+
+// console.log(arr_1.indexOf("orange")); // returns index of a specific element of an array
+ 
+// const arr_3 = arr_1.join(arr_2) // concatenates two arrays and returns string
+
+// console.log(typeof Array(arr_3)); 
+
+// slice and splice
+ 
+// console.log(arr_2);
+
+// console.log(arr_2.slice(1,3)); // only slicing is done
+// console.log(arr_2.splice(1,2)); // splitting and slicing both are done together
+// console.log(arr_2); 
+
+// array in array
+
